aad78f0862d240c34a2d623c6fbc65f1b15bf05a
bfernandodco <bfernandodco@protonmail.com>
2023-08-31 08:32:21 -0300

    Initial commit - Implementado a estrutura e a super classe.

---
+++ model/Cinema.py
@@ -0,0 +1,26 @@
+class Cinema:
+
+    def __init__(self,nome, ano):
+        self._nome = nome.title()
+        self._ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.tittle()
+
+    @property
+    def ano(self):
+        return self._ano
+
+    @ano.setter
+    def ano(self, novo_ano):
+        self._ano = novo_ano
+
+    @property
+    def likes(self):
+        return self._likes
